6ade699291d86f00ab25569b39b8953b6d43041f
Evgeny Zimin <nullptr>
2023-02-03 21:46:00 +0400

    no message

--- lab7/DjangoProject/main/models.py
+++ lab7/DjangoProject/main/models.py
@@ -5,10 +5,27 @@ class Table1(models.Model):
     fio = models.CharField('ФИО', max_length=100)
     date = models.DateTimeField('Дата обращения')
     text = models.CharField('Тип обращения', max_length=300)
+    clientId = models.IntegerField('id клиента', default=0)
+    bankId = models.IntegerField('id банка', default=0)
 
     def __str__(self):
         return self.fio
 
     class Meta:
-        verbose_name = 'Таблица 1'
-        verbose_name_plural = 'Таблицы 1'
+        verbose_name = 'История обращений'
+        verbose_name_plural = 'История обращений'
+
+
+class Table2(models.Model):
+    fio = models.CharField('ФИО', max_length=100)
+    date = models.DateTimeField('Дата обращения')
+    text = models.CharField('Тип обращения', max_length=300)
+    clientId = models.IntegerField('id клиента', default=0)
+    bankId = models.IntegerField('id банка', default=0)
+
+    def __str__(self):
+        return self.fio
+
+    class Meta:
+        verbose_name = 'История обращений'
+        verbose_name_plural = 'История обращений'
--- lab7/DjangoProject/main/urls.py
+++ lab7/DjangoProject/main/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('main', views.main, name='home'),
-    path('table1', views.table1, name='table1')
+    path('table1', views.table1, name='table1'),
+    path('table2', views.table1, name='table2'),
+    path('table3', views.table1, name='table3'),
+    path('table4', views.table1, name='table4'),
+    path('table5', views.table1, name='table5')
 ]
